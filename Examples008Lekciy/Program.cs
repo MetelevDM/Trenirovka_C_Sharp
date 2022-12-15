ffdb3d6b2fcaaaa853e24cc7c131283e92a6d2cc
MetelevDM <metelevdm88@gmail.com>
2022-12-15 16:17:29 +0400

    Новые функции

---
+++ Examples008Lekciy/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " будет " + result);
+
+
+Console.WriteLine("Введите число");
+string stringNumber = Console.ReadLine();
+int number;
+while (!int.TryParse(stringNumber, out number))
+{
+    Console.WriteLine("Ошибка! Введите одно целое число");
+    stringNumber = Console.ReadLine().Trim();
+}
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());  //int.Parse()
+int result = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " будет " + result);
+
