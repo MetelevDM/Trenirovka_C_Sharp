82ea0962943eb472b88764f8db143346d60c8af5
MetelevDM <metelevdm88@gmail.com>
2022-12-15 17:22:58 +0400

    Отредактировал код по первой задаче

--- Home_Work_C#/Program.cs
+++ Home_Work_C#/Program.cs
@@ -6,5 +6,12 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число = ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 int Max = number1;
-if (Max < number2) Max = number2;
+int Min = number2;
+if (Max < number2) 
+    Max = number2; 
+    Min=number1;
+//else Min=number2;Max = number2
 Console.WriteLine("Наиболшее число = "+Max);
+Console.WriteLine("Наименьшее число = "+Min);
+
+
