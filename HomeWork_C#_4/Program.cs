c00253b3b4d744ebbdc70a369e6557d0a4fce6d0
MetelevDM <metelevdm88@gmail.com>
2022-12-15 23:44:59 +0400

    Начал писать код

---
+++ HomeWork_C#_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите четное число: ");
+int Num=Convert.ToInt32(Console.Read());
+string Num2="q";
+if (Num % 2==0)
+{
+ while (Num>2)
+ Num2= "Num"+"Num2";
+ Num= Num-2;        
+}
+Console.Write(Num2);
