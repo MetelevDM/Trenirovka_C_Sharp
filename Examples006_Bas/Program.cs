0c8ef4755f2ee126bc463a7b1f23f0453a9b0f6f
MetelevDM <metelevdm88@gmail.com>
2022-12-15 08:40:32 +0400

    Examples006_Bas

---
+++ Examples006_Bas/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int a = 3;
+int b = 5;
+int c = 2;
+int d = 1;
+int e = 4;
+int max = a;
+if (max < a) a = max;
+if (max < b) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Наибольшее число: ");
+Console.WriteLine(max);
