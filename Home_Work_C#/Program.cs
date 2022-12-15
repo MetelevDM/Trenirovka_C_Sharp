a251b4721ccfe072a8a13d560084428bfc436f37
MetelevDM <metelevdm88@gmail.com>
2022-12-15 16:39:09 +0400

    Сделал код по первой задаче

---
+++ Home_Work_C#/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число = ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число = ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int Max = number1;
+if (Max < number2) Max = number2;
+Console.WriteLine("Наиболшее число = "+Max);
