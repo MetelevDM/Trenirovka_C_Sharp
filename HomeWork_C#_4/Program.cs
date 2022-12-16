831e354b23b4b99642601a45e17e511a2e7e4adc
MetelevDM <metelevdm88@gmail.com>
2022-12-16 12:19:44 +0400

    Доделал

--- HomeWork_C#_4/Program.cs
+++ HomeWork_C#_4/Program.cs
@@ -1,10 +1,60 @@
-﻿Console.Write("Введите четное число: ");
-int Num=Convert.ToInt32(Console.Read());
-string Num2="q";
-if (Num % 2==0)
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int Num=Convert.ToInt32(Console.ReadLine());
+if (Num==0)
 {
- while (Num>2)
- Num2= "Num"+"Num2";
- Num= Num-2;        
+    Console.WriteLine("Введите другое число");
 }
-Console.Write(Num2);
+if (Num==1)
+{
+    Console.WriteLine("Введите другое число");
+}
+if (Num==-1)
+{
+    Console.WriteLine("Введите другое число");
+}
+if (Num>1)
+{
+    if (Num%2==0)
+    {
+        Console.WriteLine(Num);
+        while(Num!=2)
+        {
+            Num=Num-2;
+            Console.WriteLine(Num);
+        }
+    }
+    else
+    {
+      int X=Num-1;
+      Console.WriteLine(X);
+      while (X!=2)
+      {
+        X=X-2;
+        Console.WriteLine(X);
+      }
+    }
+}
+if (Num<-1)
+{
+    if (Num%2==0)
+    {
+        Console.WriteLine(Num);
+        while(Num!=-2)
+        {
+            Num=Num+2;
+            Console.WriteLine(Num);
+        }
+    }
+    else
+    {
+      int X=Num+1;
+      Console.WriteLine(X);
+      while (X!=-2)
+      {
+        X=X+2;
+        Console.WriteLine(X);
+      }
+    }
+}
