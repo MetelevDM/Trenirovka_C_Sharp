98564230c6094ca81304e4fcf506a768deea7ae2
MetelevDM <metelevdm88@gmail.com>
2022-12-18 22:51:30 +0400

    ГОТОВО

---
+++ Examples009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = { 21, 32, 34, 24, 156, 16, 337, 28, 19 };
+
+int max= Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+//int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Examples010_MetodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 22, 12, 25, 14, 25, 56, 17, 48 };
+int n = array.Length;
+int find = 25;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
