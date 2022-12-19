01ec19d4cfb69aef63a3c694febffb9e4060b035
MetelevDM <metelevdm88@gmail.com>
2022-12-19 10:41:00 +0400

    Код написал частично по памяти

--- Examples011_ArrayLibrary/Program.cs
+++ Examples011_ArrayLibrary/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)
+﻿/* void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
@@ -45,4 +45,48 @@ array[5]=6;
 PrintArray(array);
 Console.WriteLine();
 int pos = IndexOf(array, 6);
-Console.WriteLine(pos);
+Console.WriteLine(pos); */
+
+
+
+
+
+
+void FillArray(int[] NewT)
+{
+    int Len = NewT.Length;
+    int index = 0;
+    while (index < Len)
+    {
+        NewT[index] = new Random().Next(1, 10);
+        Console.WriteLine(NewT[index]);
+        index++;
+    }
+
+}
+
+int[] array = new int[10];
+
+
+int Finde(int[] bob, int number)
+{
+    int q = bob.Length;
+    int index1 = 0;
+    int verny = 0;
+    while (index1 < q)
+    {
+        if (bob[index1] == number)
+        {
+            verny = index1;
+            break;
+        }
+
+        index1++;
+    }
+    return verny;
+}
+
+FillArray(array);
+Console.WriteLine("Вот оно");
+int pop = Finde(array, 4);
+Console.WriteLine(pop);
