d464170f0c59c2624b87725d60057abe1d71504a
MetelevDM <metelevdm88@gmail.com>
2022-12-20 12:05:54 +0400

    Второй вариант

--- HomeWorkEx15/Program.cs
+++ HomeWorkEx15/Program.cs
@@ -6,6 +6,6 @@
 
 Console.WriteLine(" Введите число от 1 до 7 включительно, где цифра соответствует дню недели: ");
 int N= Convert.ToInt32(Console.ReadLine());
-if (N>7 || N<1) Console.WriteLine("Вы ввели неверное число");
+
 if (N>0 && N<6) Console.WriteLine("Сегодня не выходной");
 else Console.WriteLine("Сегодня выходой");
