f20c71b91b4b1938fd855e20e799d287128792c7
MetelevDM <metelevdm88@gmail.com>
2023-01-12 14:16:14 +0400

    Готово

---
+++ HomeWorkEx25/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+
+
+void power()
+{
+Console.WriteLine("Веедите число A: ");
+int numberOne= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Веедите число B: ");
+int numberTwo= Convert.ToInt32(Console.ReadLine());
+int power=1;
+for(int i=1; i<=numberTwo;i++)
+{
+    power=power*numberOne;
+}
+
+Console.WriteLine(power);
+}
+
+power();
