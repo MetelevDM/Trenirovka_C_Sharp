57d162a893a6322a7a86a66640547a983feaa2f0
MetelevDM <metelevdm88@gmail.com>
2023-01-17 12:14:12 +0400

    Готово

---
+++ HomeWorkEx38/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 38: 
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементом массива. */
+
+int[] newArray()
+{
+    Console.WriteLine("Введите размер массива: ");
+    int lengthArray = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[lengthArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+int[] array = newArray();
+int numberMax = array[0];
+int numberMin = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (numberMax < array[i]) numberMax = array[i];
+    else
+    {
+        if (numberMin > array[i]) numberMin = array[i];
+    }
+}
+int different = numberMax - numberMin;
+Console.WriteLine($"Разница между максимальным числом {numberMax} и минимальным числом {numberMin} в массиве [{String.Join(',', array)}] равна {different}");
