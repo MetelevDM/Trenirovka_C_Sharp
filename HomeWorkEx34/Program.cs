76c60cecc3bfbad7c7a3d431d357ca47d7292f0b
MetelevDM <metelevdm88@gmail.com>
2023-01-17 11:09:04 +0400

    Готово

---
+++ HomeWorkEx34/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+ */
+
+
+
+int[] newRandonArray(int count, int start, int end)
+{
+    int[] randomArray = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        randomArray[i] = new Random().Next(start, end);
+    }
+    return randomArray;
+}
+
+int[] array = newRandonArray(10, 100, 999);
+int number = 0;
+
+for (int i = 0; i < array.Length; i++)
+{    
+    if (array[i] % 2 == 0) number = number + 1;
+}
+
+
+
+
+
+Console.WriteLine($" Количество четных числе в массиве [ {String.Join(", ", array)} ] равно {number}");
