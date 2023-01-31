e62be7ec1c7e5030fe3ca89349004b8890edb44a
MetelevDM <metelevdm88@gmail.com>
2023-01-31 13:12:17 +0400

    Готово

---
+++ HomeWorkEx68/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int Akker(int m, int n)
+{
+    int res = 0;
+    if (m == 0) res = n + 1;
+    if (m > 0 && n == 0) res = Akker(m - 1, 1);
+    if (m > 0 && n > 0) res = Akker(m - 1, Akker(m, n - 1));
+    return res;
+}
+
+Console.WriteLine($"{Akker(3,2)}");
+
